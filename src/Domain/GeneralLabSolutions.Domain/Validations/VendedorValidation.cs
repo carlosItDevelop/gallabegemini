cf7ee0e490c6101ce3d7f90fab0606d255a0303b
Carlos A Santos <carlos.itdeveloper@gmail.com>
2025-06-29 02:20:51 -0300

    Add Documento in VendedorValidation

--- src/Domain/GeneralLabSolutions.Domain/Validations/VendedorValidation.cs
+++ src/Domain/GeneralLabSolutions.Domain/Validations/VendedorValidation.cs
@@ -1,5 +1,8 @@
 using FluentValidation;
 using GeneralLabSolutions.Domain.Entities;
+using GeneralLabSolutions.Domain.Entities.Base;
+using GeneralLabSolutions.Domain.Enums;
+using GeneralLabSolutions.Domain.Extensions.Extending;
 
 namespace GeneralLabSolutions.Domain.Validations
 {
@@ -21,6 +24,31 @@ namespace GeneralLabSolutions.Domain.Validations
 
             RuleFor(v => v.TipoDePessoa)
                 .IsInEnum().WithMessage("O campo Tipo de Pessoa precisa ser válido");
+
+            // ───── Documento: CPF / CNPJ ───────────────────────────────
+            RuleFor(c => c.Documento)
+                .NotEmpty().WithMessage("O campo {PropertyName} é obrigatório.");
+
+            When(c => c.TipoDePessoa == TipoDePessoa.Fisica, () =>
+            {
+                RuleFor(c => c.Documento.ApenasNumeros().Length) // MODIFICADO
+                    .Equal(CpfValidacao.TamanhoCpf)
+                    .WithMessage($"O CPF deve conter {CpfValidacao.TamanhoCpf} dígitos.");
+
+                RuleFor(c => CpfValidacao.Validar(c.Documento)) // CpfValidacao.Validar internamente usa .ApenasNumeros()
+                    .Equal(true).WithMessage("O CPF fornecido é inválido.");
+            });
+
+            When(c => c.TipoDePessoa == TipoDePessoa.Juridica, () =>
+            {
+                RuleFor(c => c.Documento.ApenasNumeros().Length) // MODIFICADO
+                    .Equal(CnpjValidacao.TamanhoCnpj)
+                    .WithMessage($"O CNPJ deve conter {CnpjValidacao.TamanhoCnpj} dígitos.");
+
+                RuleFor(c => CnpjValidacao.Validar(c.Documento)) // CnpjValidacao.Validar internamente usa .ApenasNumeros()
+                    .Equal(true).WithMessage("O CNPJ fornecido é inválido.");
+            });
+
         }
     }
 }
